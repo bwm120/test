e07a0f5da9ee2a10d3ddc5e06b3a60fc4124d1ca
bileee <lebidepzai@gmail.com>
2024-11-08 02:47:26 +0700

    FPT-LVB

--- scripts.js
+++ scripts.js
@@ -5,3 +5,20 @@ function showSection(sectionId) {
     });
     document.getElementById(sectionId).classList.add('visible');
   }
+  const fadeInSections = document.querySelectorAll('.section');
+
+  const options = {
+    threshold: 0.1
+  };
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('roll');
+      }
+    });
+  }, options);  
+  
+  fadeInSections.forEach(section => {
+    observer.observe(section);
+  });
